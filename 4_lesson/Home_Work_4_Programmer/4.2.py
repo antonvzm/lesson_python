61c75b676688c3beefa1b54c567074a13dc1966a
«Anton» <antonvzm@gmail.com>
2022-12-29 00:32:35 +0300

    push

---
+++ 4_lesson/Home_Work_4_Programmer/4.2.py
@@ -0,0 +1,20 @@
+# 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+# in
+# 54
+
+# out
+# [2, 3, 3, 3]
+
+number = int(input('in\n'))
+i = 2
+list_numbers = []
+old = number
+while i <= number:
+    if number % i == 0:
+        list_numbers.append(i)
+        number //= i
+        i = 2
+    else:
+        i += 1
+print('out\n', list_numbers)
